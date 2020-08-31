75ffe0320c857425e2fe58ed64394fcdef3876fb
zebrakirin <65844930+zebrakirin@users.noreply.github.com>
2020-08-31 17:36:16 +0900

    js Home transparent

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 "use strict";
 
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
+
 // make navbar transparent when it is on the top
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
@@ -28,7 +33,9 @@ contactmebtn.addEventListener("click", (event) => {
   scrollIntoView("#contact");
 });
 
-function scrollIntoView(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
-}
+//Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
