e4866bdbf8df97a7423039321df6f7a9ca9c248c
zebrakirin <65844930+zebrakirin@users.noreply.github.com>
2020-09-01 13:28:30 +0900

    js project filtering & ani

--- main.js
+++ main.js
@@ -47,11 +47,26 @@ document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
-// //project
-// const workBtnContainer = document.querySelector(".work__categories");
-// const projectContainer = document.querySelector(".work__projects");
-// const projects = document.querySelector(".project");
-// workBtnContainer.addEventListener("click", (e) => {
-//   const filter : e.target.dataset.filter;
-//   console.log(filter);
-// });
+//project
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+workBtnContainer.addEventListener("click", (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  projectContainer.classList.add("anim-out");
+  setTimeout(() => {
+    projects.forEach((project) => {
+      console.log(project.dataset.type);
+      if (filter === "*" || filter === project.dataset.type) {
+        project.classList.remove("invisible");
+      } else {
+        project.classList.add("invisible");
+      }
+    });
+
+    projectContainer.classList.remove("anim-out");
+  }, 300);
+});
